485021d192f9c4ffc2d6b3cf79570aeefe881944
Juan Cruz <juancruzf017@gmail.com>
2023-03-30 12:59:30 -0300

    Add GET, PUT Y PATCH /category/{id}

--- expense_management/api/urls.py
+++ expense_management/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from expense_management.api.views import CategoryList
+from expense_management.api.views import CategoryList, CategoryGetUpdate
 
 urlpatterns = [
-    path('categories',CategoryList.as_view(), name='categories')
+    path('categories',CategoryList.as_view(), name='categories'),
+    path('category/<int:pk>',CategoryGetUpdate.as_view(), name='category-get-update')
 ]
