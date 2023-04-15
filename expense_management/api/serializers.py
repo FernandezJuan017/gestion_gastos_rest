11f7cf275578ec5c9f6684edc52a62dbfa2e9c6e
Juan Cruz <juancruzf017@gmail.com>
2023-04-14 21:37:09 -0300

    Quito del modelo los datos de tipo enum
    Creo la misma relacion en forma de tablas

--- expense_management/api/serializers.py
+++ expense_management/api/serializers.py
@@ -1,58 +1,22 @@
 from rest_framework import serializers
-from expense_management.models import Category
-from django_enum_choices.fields import EnumChoiceField
+from expense_management.models import Category_Type, Category
 
+#Category Type
+class CategoryTypeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Category_Type
+        fields = '__all__'
+        
 #Category
 class CategorySerializer(serializers.ModelSerializer):
-    #type = serializers.SerializerMethodField()
-    #type = serializers.CharField(source='type.value')
-    type = serializers.ChoiceField(source = 'type.value' ,choices=[(choice.value, choice.name) for choice in Category.TypeCategory])
-    #type = serializers.ChoiceField(choices=[(choice.name, choice.value) for choice in Category.TypeCategory])
-
+    #type = CategoryTypeSerializer()
     class Meta:
         model = Category
         fields = '__all__' #['id', 'name', 'description', 'type', 'parent_category']
         
-    def get_type(self, obj):
-        return obj.get_type_display()
-    
-    def getCategoryType(strType: str):
-        #Si no puede retornar un TypeCategory, devuelve un string vacio
-        #valor_por_defecto = ""
         
-        if len(strType) > 0:
-            if strType.upper() == Category.TypeCategory.INGRESOS.value:
-                return Category.TypeCategory.INGRESOS
-            elif strType.upper() == Category.TypeCategory.GASTOS.value:
-                return Category.TypeCategory.GASTOS
-            
-        return Category.TypeCategory.INGRESOS
-    
-    def create(self, validated_data):
-        str_type = str(validated_data.pop('type').get('value'))
-
-        type_value = CategorySerializer.getCategoryType(str_type)
-   
-        category = Category.objects.create(type=type_value, **validated_data)
-        return category
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name',instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.parent_category = validated_data.get('parent_category', instance.parent_category)
-
-        # Manejo del campo 'type'
-        str_type = str(validated_data.get('type', '').get('value'))
-    
-        if str_type and len(str_type) > 0 :
-                   
-            if str_type.upper() == Category.TypeCategory.INGRESOS.value:
-                instance.type = Category.TypeCategory.INGRESOS
-            elif str_type.upper() == Category.TypeCategory.GASTOS.value:
-                instance.type = Category.TypeCategory.GASTOS
-                
-        instance.save()
-        return instance
+        
     
    
     
--- expense_management/models.py
+++ expense_management/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import datetime
-from django_enum_choices.fields import Enum
-from django_enum_choices.fields import EnumChoiceField
+# from django_enum_choices.fields import Enum
+# from django_enum_choices.fields import EnumChoiceField
 
    
 # Users
@@ -10,14 +10,20 @@ class User(AbstractUser):
     @property
     def fullname(self) -> str:
         return f"{self.surmame}, {self.name}"
+    
+#CategyTypes        
+class Category_Type(models.Model):
+    name = models.CharField(max_length=50)
         
+    def __str__(self) -> str:
+        return self.name 
     
 #Categories
 class Category(models.Model):
     
-    class TypeCategory(Enum):
-        INGRESOS = "INGRESO"
-        GASTOS = "GASTO"
+    # class TypeCategory(Enum):
+    #     INGRESOS = "INGRESO"
+    #     GASTOS = "GASTO"
         
     class Meta:
         verbose_name_plural = 'Categories'
@@ -27,7 +33,7 @@ class Category(models.Model):
                                         null= True, blank= True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=150, null=True, blank= True)
-    type = EnumChoiceField(TypeCategory, default=TypeCategory.INGRESOS)
+    type = models.ForeignKey(Category_Type, on_delete=models.CASCADE, related_name='categories') #EnumChoiceField(TypeCategory, default=TypeCategory.INGRESOS)
                
     def __str__(self) -> str:
         return self.name
@@ -44,18 +50,25 @@ class Account(models.Model):
     def __str__(self) -> str:
         return "%s - %s" % (self.name, self.user.fullname)
     
+#EffectTypes
+class EffectType(models.Model):
+    name = models.CharField(max_length=50)
+        
+    def __str__(self) -> str:
+        return self.name 
+
 #Transaction_Types
 class TransactionType(models.Model):
     
-    class TypeEffect(Enum):
-        INGRESO = "INGRESO"
-        EGRESO = "EGRESO"
+    # class TypeEffect(Enum):
+    #     INGRESO = "INGRESO"
+    #     EGRESO = "EGRESO"
         
     type = models.CharField(max_length=100)
-    effect = EnumChoiceField(TypeEffect, default=TypeEffect.INGRESO)
+    effect = models.ForeignKey(EffectType,on_delete=models.CASCADE) #EnumChoiceField(TypeEffect, default=TypeEffect.INGRESO)
     
     def __str__(self) -> str:
-        return "%s - Efecto: %s" % (self.type, self.effect)
+        return "%s - Efecto: %s" % (self.type, self.effect.name)
     
  #LabelTransaction
 class LabelTransaction(models.Model):
