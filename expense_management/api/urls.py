9f45f3b28cf1318d3d8fa4433f20b0be1f521975
Juan Cruz <juancruzf017@gmail.com>
2023-05-08 19:16:05 -0300

    Add GET /api/accounts
    Listado de cuentas.

--- expense_management/api/urls.py
+++ expense_management/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from expense_management.api.views import CategoryList, CategoryGetUpdate, CategoryCreate
+from expense_management.api.views import CategoryList, CategoryGetUpdate, CategoryCreate, AccountList 
 
 urlpatterns = [
     path('categories', CategoryList.as_view(), name='categories'),
     path('category', CategoryCreate.as_view(), name = 'category-create'),
-    path('category/<int:pk>',CategoryGetUpdate.as_view(), name='category-get-update')
+    path('category/<int:pk>',CategoryGetUpdate.as_view(), name='category-get-update'),
+    path('accounts',AccountList.as_view(), name = 'accounts')
 ]
